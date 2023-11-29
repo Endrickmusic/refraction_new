4998949884de29e535c2609ba9b174fbf515c932
Christian Hohenbild <mail@christianhohenbild.de>
2023-11-30 00:11:30 +0100

    add gamma correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { OrbitControls, useFBO } from "@react-three/drei";
 import { Canvas, useFrame } from "@react-three/fiber";
+import { Leva, folder, useControls } from "leva";
 import { useMemo, useRef } from "react";
 import * as THREE from "three";
 import { v4 as uuidv4 } from "uuid";
-import { range } from './utils'
+import { range } from './utils';
 import './index.css';
 
 import vertexShader from './vertexShader';
@@ -17,9 +18,50 @@ const Geometries = () => {
   // This is our main render target where we'll render and store the scene as a texture
   const mainRenderTarget = useFBO();
 
+  const {
+    iorR,
+    iorG,
+    iorB,
+    chromaticAberration,
+    refraction
+  } = useControls({
+    ior: folder({
+      iorR: { min: 1.0, max: 2.333, step: 0.001, value: 1.15 },
+      iorG: { min: 1.0, max: 2.333, step: 0.001, value: 1.18 },
+      iorB: { min: 1.0, max: 2.333, step: 0.001, value: 1.22 },
+    }),
+    chromaticAberration: {
+      value: 0.5,
+      min: 0,
+      max: 1.5,
+      step: 0.01,
+    },
+    refraction: {
+      value: 0.4,
+      min: 0,
+      max: 1,
+      step: 0.01,
+    },
+  })
+
   const uniforms = useMemo(() => ({
     uTexture: {
-      value: null,
+      value: null
+    },
+    uIorR: {
+      value: 1.0,
+    },
+    uIorG: {
+      value: 1.0,
+    },
+    uIorB: {
+      value: 1.0,
+    },
+    uRefractPower: {
+      value: 0.2,
+    },
+    uChromaticAberration: {
+      value: 1.0
     },
     winResolution: {
       value: new THREE.Vector2(
@@ -39,20 +81,27 @@ const Geometries = () => {
 
     gl.setRenderTarget(null);
     mesh.current.visible = true;
+
+    mesh.current.material.uniforms.uIorR.value = iorR;
+    mesh.current.material.uniforms.uIorG.value = iorG;
+    mesh.current.material.uniforms.uIorB.value = iorB;
+
+    mesh.current.material.uniforms.uChromaticAberration.value = chromaticAberration;
+    mesh.current.material.uniforms.uRefractPower.value = refraction;
   });
 
-  const columns = range(-7.5, 7.5, 2.5);
-  const rows = range(-7.5, 7.5, 2.5);
+  const columns = range(-12.5, 12.5, 2.5);
+  const rows = range(-12.5, 12.5, 2.5);
 
   return (
     <>
-      <color attach="background" args={["grey"]} />
+      <color attach="background" args={["white"]} />
       <group ref={backgroundGroup}>
         {columns.map((col, i) =>
           rows.map((row, j) => (
             <mesh position={[col, row, -4]}>
-              <icosahedronGeometry args={[0.333, 8]} />
-              <meshStandardMaterial color="white" />
+              <boxGeometry args={[0.5, .5, .5]} />
+              <meshStandardMaterial color="grey" />
             </mesh>
           ))
         )}
@@ -72,14 +121,15 @@ const Geometries = () => {
 
 const Scene = () => {
   return (
-    <Canvas camera={{ position: [0, 0, 6] }} dpr={[1, 2]}>
-      <ambientLight intensity={1.4} />
-      <Geometries />
-      <OrbitControls />
-    </Canvas>
+    <>
+      <Leva collapsed />
+      <Canvas camera={{ position: [-3, 0, 6] }} dpr={[1, 2]}>
+        <ambientLight intensity={1.4} />
+        <Geometries />
+        <OrbitControls />
+      </Canvas>
+    </>
   );
 };
 
-
 export default Scene;
-
